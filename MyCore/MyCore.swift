ffccffad4c36682e6ef54fe268fca6c2bde511cd
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 16:16:43 +0700

    commit

---
+++ MyCore/MyCore.swift
@@ -0,0 +1,17 @@
+//
+//  MyCore.swift
+//  MyCore
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+import Foundation
+
+public class MyCore {
+    
+    public static let sharede = MyCore()
+    
+    public func startMyCore() {
+        print("get My Core")
+    }
+}
